02d6266fec727bc2741527781067e0255d22e6ad
mwanawabangona <ellisallday@gmail.com>
2021-12-11 10:54:10 -0800

    Fix bug

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/Books/books';
+import { removeBook } from '../redux/books/books';
 
 const BookItem = ({ book }) => {
   const {
